d3b376bbc99b3c758b71916f169fba64a058bbe3
mohakj22 <mohakjain.ps12@gmail.com>
2024-07-27 16:26:37 +0530

    cpp template

---
+++ demo.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define all(v) (v).begin(), (v).end()
+#define size(x) ((int)(x).size())
+#define fast_io() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int, int>
+#define vpii vector<pii>
+#define umap unordered_map
+#define uset unordered_set
+#define mapi unordered_map<int, int>
+#define mapii unordered_map<int, pair<int, int>>
+
+void solve();
+
+int main() {
+    fast_io();
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
+void solve() {
+    // Your code here
+}
