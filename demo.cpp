765769a083046c86119ef261072e6ff30cef382c
Mohak Jain <118297185+mohakj22@users.noreply.github.com>
2024-08-25 09:31:45 +0530

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -7,31 +7,53 @@ using namespace std;
 #define mp make_pair
 #define all(v) (v).begin(), (v).end()
 #define size(x) ((int)(x).size())
-#define fast_io() ios::sync_with_stdio(false); cin.tie(nullptr);
+#define fast_io()                \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
 #define vl vector<ll>
-#define vi vector<int>
+#define vi vector<ll>
 #define vvi vector<vi>
-#define pii pair<int, int>
+#define pii pair<ll, ll>
 #define vpii vector<pii>
 #define umap unordered_map
 #define uset unordered_set
-#define mapi unordered_map<int, int>
-#define mapii unordered_map<int, pair<int, int>>
+#define mapi unordered_map<ll, ll>
+#define mapii unordered_map<ll, pair<ll, ll>>
 
-void solve();
+#define inputArray(arr, n)       \
+    for (ll i = 0; i < n; i++)   \
+    {                            \
+        ll x;                    \
+        cin >> x;                \
+        arr.pb(x);               \
+    }
 
-int main() {
+#define printArray(arr)          \
+    for (ll x : arr)             \
+    {                            \
+        cout << x << " ";        \
+    }                            \
+    cout << "\n";
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> nums;
+    inputArray(nums, n);
+    printArray(nums);
+}
+
+int main()
+{
     fast_io();
 
-    int t;
+    ll t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         solve();
     }
 
     return 0;
-}
-
-void solve() {
-    // Your code here
 }
